8823cbb7753a17be5b381d4c8cad952faf09084c
patrickbynder <patrick.bakker@bynder.com>
2023-02-02 16:13:50 +0100

    redo remove commented code and console.log

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,7 @@ import { useDispatch, useSelector } from './state/store';
 function App() {
     const { tasks } = useSelector((state) => state.todoapp);
     const dispatch = useDispatch();
-    //const { tasks, dispatch } = useStore();
-    console.log('hello');
     function onAdd(title: string) {
-        // dispatch({ type: 'ADD_TASK', payload: { title } });
         dispatch(actions.todoAdded(title));
     }
 
@@ -20,17 +17,14 @@ function App() {
     }
 
     function onDelete(id: string) {
-        //dispatch({ type: 'DELETE_TASK', payload: { id } });
         dispatch(actions.taskIsDeleted({ taskId: id }));
     }
 
     function handleOnChange(done: boolean, taskId: string) {
-        //dispatch({ type: 'CHANGE_FINISHED', payload: { done, taskId } });
         dispatch(actions.todoIsFinished({ done, taskId }));
     }
 
     function OnEditTitle(editTitle: string, taskId: string) {
-        //dispatch({ type: 'EDIT_TASK', payload: { editTitle, taskId } });
         dispatch(actions.taskNameEdited({ editTitle, taskId }));
     }
 
